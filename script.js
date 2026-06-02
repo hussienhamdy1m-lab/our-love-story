9defa69aed5d46e50242bf5946f2211c77162d38
hussienhamdy1m-lab <hussienhamdy1m@gmail.com>
2026-06-02 23:39:28 +0300

    Add files via upload
    First version ❤️

---
+++ script.js
@@ -0,0 +1,31 @@
+const sections =
+document.querySelectorAll(".memory");
+
+const observer =
+new IntersectionObserver(
+
+(entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},
+
+{
+threshold:.25
+}
+
+);
+
+sections.forEach(section=>{
+
+observer.observe(section);
+
+});
