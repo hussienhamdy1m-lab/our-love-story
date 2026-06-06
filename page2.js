fb429f50b94ae64ecf8b8db450dc1732d47e8e47
hussienhamdy1m-lab <hussienhamdy1m@gmail.com>
2026-06-06 19:24:45 +0300

    Update page2.js

--- page2.js
+++ page2.js
@@ -2,17 +2,32 @@ let currentStoryIndex = 0;
 const stories = document.querySelectorAll('.story-page');
 
 function nextStory() {
-    // إخفاء الصفحة الحالية بنعومة
     stories[currentStoryIndex].classList.remove('active');
-    
-    // الانتقال للفصل القادم
     currentStoryIndex++;
-    
-    // لو الحكايات خلصت نرجع نعيد من الأول
     if (currentStoryIndex >= stories.length) {
         currentStoryIndex = 0;
     }
-    
-    // إظهار الصفحة الجديدة بأنيميشن مريح للعين
     stories[currentStoryIndex].classList.add('active');
+}
+
+// التحكم في الدخول السينمائي للكتاب
+function openBookTransition() {
+    const closedScene = document.getElementById('closed-book-scene');
+    const closedImg = document.querySelector('.closed-book-img');
+    const clickHint = document.querySelector('.click-hint');
+    const openScene = document.getElementById('open-book-scene');
+
+    // إخفاء النص
+    clickHint.style.opacity = '0';
+    
+    // عمل زووم سينمائي للصورة كأننا بندخل جواها
+    closedImg.classList.add('dive-into-book');
+
+    // بعد ثانية ونص (مدة الزووم)، نظهر الكتاب المفتوح
+    setTimeout(() => {
+        closedScene.style.display = 'none'; 
+        openScene.style.display = 'flex'; 
+        void openScene.offsetWidth; // تحديث فوري للشاشة
+        openScene.style.opacity = '1'; 
+    }, 1200);
 }
