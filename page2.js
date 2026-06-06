274254f20707e74b07c9f01282bb556c1416556e
hussienhamdy1m-lab <hussienhamdy1m@gmail.com>
2026-06-06 16:52:25 +0300

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,35 +1,27 @@
-let currentClassIndex = 0;
-// بنجيب كل الصفحات المرصوصة جوة الكتاب
-const pages = document.querySelectorAll('.page');
+const leaves = document.querySelectorAll('.leaf');
+let currentLeaf = 0;
 
-function flipPage() {
-    // لو لسه مخلصناش صفحات الكتاب كلها
-    if (currentClassIndex < pages.length) {
-        // نقلب الصفحة الحالية لورا (بإضافة كلاس الـ CSS)
-        pages[currentClassIndex].classList.add('flipped');
-        
-        // نرفع الـ Z-Index للصفحة اللي اتقلبت عشان متبوظش ترتيب الرؤية
-        pages[currentClassIndex].style.zIndex = currentClassIndex;
-        
-        currentClassIndex++;
-    } else {
-        // لو الكتاب خلص وعايزة تقفله خالص من الأول وتفتحه تاني (ريست)
-        for (let i = 0; i < pages.length; i++) {
-            pages[i].classList.remove('flipped');
-            // بنرجع الـ zIndex للوضع الأصلي (الغلاف هو اللي فوق)
-            pages[i].style.zIndex = pages.length - i;
-        }
-        currentClassIndex = 0;
-    }
-}
-
-// دالة مبدئية لترتيب صفحات الكتاب فوق بعضها في الأول
 function initBook() {
-    for (let i = 0; i < pages.length; i++) {
-        // الغلاف (الصفحة 0) بياخد أعلى z-index، والصفحة اللي بعدها أقل، وهكذا
-        pages[i].style.zIndex = pages.length - i;
+    for (let i = 0; i < leaves.length; i++) {
+        leaves[i].style.zIndex = leaves.length - i;
+    }
+}
+initBook();
+
+function flipNext(page) {
+    if (currentLeaf < leaves.length) {
+        let leafToFlip = leaves[currentLeaf];
+        leafToFlip.classList.add('flipped');
+        leafToFlip.style.zIndex = currentLeaf + 1; 
+        currentLeaf++;
     }
 }
 
-// تشغيل الترتيب المبدئي
-initBook();
+function flipPrev(page) {
+    if (currentLeaf > 0) {
+        currentLeaf--;
+        let leafToFlip = leaves[currentLeaf];
+        leafToFlip.classList.remove('flipped');
+        leafToFlip.style.zIndex = leaves.length - currentLeaf;
+    }
+}
